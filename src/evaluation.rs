93f0292768e928174b6ac18b6039a7a80421efcf
Ray Hidayat <ray.hidayat@gmail.com>
2021-06-28 02:48:52 +1200

    Small penalty for taking too long

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,15 +1,18 @@
 pub use super::model::*;
 
 const LOSS_POINTS: f32 = -10000.0;
+const POINTS_PER_TICK: f32 = -0.01;
 
 pub fn evaluate(_world: &World, events: &Vec<Event>) -> f32 {
     let mut total_score: f32 = 0.0;
+
     for event in events.iter() {
         match event {
             Event::ZombieKilled { score, .. } => {
                 total_score += score;
             },
-            Event::Ending { won, .. } => {
+            Event::Ending { won, tick, .. } => {
+                total_score += POINTS_PER_TICK * (*tick as f32);
                 if !won {
                     total_score += LOSS_POINTS;
                 }
@@ -17,5 +20,6 @@ pub fn evaluate(_world: &World, events: &Vec<Event>) -> f32 {
             _ => (),
         }
     }
+
     total_score
 }
